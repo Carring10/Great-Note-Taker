7877fa0de36d002d721b20cb1ad0449b759868ae
Carrington <hartis.c99@gmail.com>
2022-03-24 20:11:48 -0400

    new notes will append to the db

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+const { randomUUID } = require("crypto");
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const util = require("util");
+const noteData = require("./db/db.json");
 // execute express
 const app = express();
 // NEVER change this
@@ -14,4 +18,45 @@ app.get("*", (req, res) => {
   // render index.js
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+// returns notes.html file
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+// return saved notes as JSON
+app.get("/api/notes", (req, res) => res.json(noteData));
+
+// new notes save on the request body, then adds it to the db.json file, then returns that new note to the client. Each note will have a unique id when it is saved.
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: randomUUID(),
+    };
+
+    const writeToFile = (destination, content) =>
+      fs.writeFile(destination, JSON.stringify(content), (err) =>
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+      );
+
+    const readAndAppend = (content, filePath) => {
+      fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const parsedData = JSON.parse(data);
+          parsedData.push(content);
+          writeToFile("./db/db.json", parsedData);
+          res.send("success!");
+        }
+      });
+    };
+    readAndAppend(newNote, "./db/db.json");
+  } else {
+    res.error("Error in adding note");
+  }
+});
+
 app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
